16a9d58558008c06e26a22e6b9f800799ace3928
syke99 <quinnmillican@gmail.com>
2022-04-02 01:42:29 -0500

    cleaner goroutine creation

--- handle_tweet.go
+++ handle_tweet.go
@@ -10,42 +10,44 @@ func handleTweet(bytes []byte) {
 		}
 		tweet = extractValues(bytes, tweet)
 
-		done := make(chan bool)
-		go func(done chan bool, tweet Tweet) {
-			for {
-				select {
-				case <-done:
-					return
-				default:
-					// Check for empty tweet.MediaUrl to
-					// prevent crash from panic in processing
-					// images
-					if tweet.MediaUrl == "" {
-						return
-					}
-
-					// Download Image
-					fileName, err := downloadImage(tweet.MediaUrl, tweet.AuthorName)
-					if err != nil {
-						fmt.Printf("%v\n", err)
-					}
-
-					// println(fileName)
-
-					// Resize the image
-					resizeImage(fileName, tweet.Text)
-					if err != nil {
-						fmt.Printf("%v\n", err)
-					}
-
-					// Generate Pattern
-
-					// Reply with Pattern
-
-					// Signal done
-					done <- true
-				}
-			}
-		}(done, tweet)
+		// Check for empty tweet.MediaUrl to
+		// prevent crash from panic in processing
+		// images
+		if tweet.MediaUrl != "" {
+			createGoRoutineForTweet(tweet)
+		}
 	}
+}
+
+func createGoRoutineForTweet(tweet Tweet) {
+	done := make(chan bool)
+	go func(done chan bool, tweet Tweet) {
+		for {
+			select {
+			case <-done:
+				return
+			default:
+				// Download Image
+				fileName, err := downloadImage(tweet.MediaUrl, tweet.AuthorName)
+				if err != nil {
+					fmt.Printf("%v\n", err)
+				}
+
+				// println(fileName)
+
+				// Resize the image
+				resizeImage(fileName, tweet.Text)
+				if err != nil {
+					fmt.Printf("%v\n", err)
+				}
+
+				// Generate Pattern
+
+				// Reply with Pattern
+
+				// Signal done
+				done <- true
+			}
+		}
+	}(done, tweet)
 }
