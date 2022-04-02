76017852d7849843efc920c8c2d913ccad9e8bac
syke99 <quinnmillican@gmail.com>
2022-04-02 01:14:05 -0500

    refactored goroutine creation into its own file (handle_tweet.go)

---
+++ handle_tweet.go
@@ -0,0 +1,51 @@
+package main
+
+import "fmt"
+
+func handleTweet(bytes []byte) {
+	// This check handles sporadic empty messages
+	if len(bytes) >= 0 {
+		tweet := Tweet{
+			Error: false,
+		}
+		tweet = extractValues(bytes, tweet)
+
+		done := make(chan bool)
+		go func(done chan bool, tweet Tweet) {
+			for {
+				select {
+				case <-done:
+					return
+				default:
+					// Check for empty tweet.MediaUrl to
+					// prevent crash from panic in processing
+					// images
+					if tweet.MediaUrl == "" {
+						return
+					}
+
+					// Download Image
+					fileName, err := downloadImage(tweet.MediaUrl, tweet.AuthorName)
+					if err != nil {
+						fmt.Printf("%v\n", err)
+					}
+
+					// println(fileName)
+
+					// Resize the image
+					resizeImage(fileName, tweet.Text)
+					if err != nil {
+						fmt.Printf("%v\n", err)
+					}
+
+					// Generate Pattern
+
+					// Reply with Pattern
+
+					// Signal done
+					done <- true
+				}
+			}
+		}(done, tweet)
+	}
+}
