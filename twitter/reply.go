feecf023461dfaf1dadc229c0d558ea3c5e373e8
syke99 <quinnmillican@gmail.com>
2022-04-10 10:08:50 -0500

    added ReplyError for error-handling later

--- twitter/reply.go
+++ twitter/reply.go
@@ -9,7 +9,6 @@ import (
 )
 
 func ReplyUrl(client Client, tweet Tweet, fileName string) {
-
 	replyId := ReplyId{
 		InReplyToStatusId: tweet.Id,
 	}
@@ -25,14 +24,11 @@ func ReplyUrl(client Client, tweet Tweet, fileName string) {
 
 	body, _ := json.Marshal(twt)
 
-	//fmt.Printf("%s\n", body)
-
 	req, err := http.NewRequest(http.MethodPost, "https://api.twitter.com/2/tweets", bytes.NewBuffer(body))
 	if err != nil {
 		fmt.Printf("%v\n", err)
 	}
 	req.Header.Set("Content-type", "application/json")
-	//req.Header.Set("Authorization", bearer)
 
 	resp, err := client.Oauth.Do(req)
 	if err != nil {
@@ -47,8 +43,37 @@ func ReplyUrl(client Client, tweet Tweet, fileName string) {
 	println(string(reader))
 }
 
-func ReplyError(client Client, tweet Tweet, fileName string) {
+func ReplyError(client Client, tweet Tweet, fileName string, replyError string) {
+	replyId := ReplyId{
+		InReplyToStatusId: tweet.Id,
+	}
 
-	// replyStatus := fmt.Sprintf("@%v Thanks for mentioning!!", userName)
+	var reply ReplyTweet
 
+	reply.Text = replyError
+	reply.ReplyToId = replyId
+
+	twt := make(map[string]interface{})
+	twt["text"] = reply.Text
+	twt["reply"] = reply.ReplyToId
+
+	body, _ := json.Marshal(twt)
+
+	req, err := http.NewRequest(http.MethodPost, "https://api.twitter.com/2/tweets", bytes.NewBuffer(body))
+	if err != nil {
+		fmt.Printf("%v\n", err)
+	}
+	req.Header.Set("Content-type", "application/json")
+
+	resp, err := client.Oauth.Do(req)
+	if err != nil {
+		fmt.Printf("%v\n", err)
+	}
+
+	reader, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Printf("%v\n", err)
+	}
+
+	println(string(reader))
 }
