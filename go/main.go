a0c4b51b8115f1548267c081ec1db6ccca7fa973
Carlo Munguia <cmunguia@denimgroup.com>
2022-05-28 21:13:59 -0500

    Vue Integration 2
    changed si-frontend to client, updated package.json / lock to reflect, killed vue gitignore, brought over data over to root gitignore, removed comment lines from main.go

--- go/main.go
+++ go/main.go
@@ -16,9 +16,6 @@ func main() {
 	<-done
 }
 
-// May need to change into expecting a bas64 encoded string
-// not sure because syscall/js doesn't provide a type cast to
-// a []byte, but does provide a type cast to String
 func processAndCreatePattern(this js.Value, args []js.Value) interface{} {
 	resImg := imgHdl.ResizeImage(args[0].String(), args[1].Bool(), args[3].Int(), args[4].Int())
 
